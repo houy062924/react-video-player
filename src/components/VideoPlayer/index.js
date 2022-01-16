93072cfa7f3c01507f90a32e4fe4be30717410b4
houy062924 <47458867+houy062924@users.noreply.github.com>
2022-01-16 15:04:40 +0800

    Feat: VideoPlayer forward / backwards / jump to / buffering function & UI

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -16,9 +16,10 @@ function VideoPlayer() {
 
   const [isPlaying, setIsPlaying] = useState(false);
   const [isFullScreen, setIsFullScreen] = useState(false);
-  const [videoProgress, setVideoProgress] = useState('00:00');
-  const [videoDuration, setVideoDuration] = useState('00:00');
+  const [videoProgressText, setVideoProgressText] = useState('00:00');
+  const [videoDurationText, setVideoDuration] = useState('00:00');
   const [progressBarWidth, setProgressBarWidth] = useState(0);
+  const [bufferBarWidth, setBufferBarWidth] = useState(0);
 
   const onLoadVideo = () => {
     const duration = getTimeAsMinSec(player.current.duration);
@@ -31,6 +32,7 @@ function VideoPlayer() {
     } else {
       player.current.play();
     }
+
     setIsPlaying((prevIsPlaying) => !prevIsPlaying);
   };
 
@@ -48,22 +50,56 @@ function VideoPlayer() {
     setIsFullScreen((prevIsFullScreen) => !prevIsFullScreen);
   };
 
-  const onUpdateProgress = (e) => {
+  const onUpdateProgressBar = (e) => {
     const progressWidth = (e.target.currentTime / e.target.duration) * 100;
     const progress = getTimeAsMinSec(e.target.currentTime);
+    const buffered = (e.target.buffered.end(0) / e.target.duration) * 100;
 
     if (e.target.ended) {
       setIsPlaying(false);
     }
 
+    if (buffered !== bufferBarWidth) {
+      setBufferBarWidth(buffered);
+    }
+
     setProgressBarWidth(progressWidth);
-    setVideoProgress(progress);
+    setVideoProgressText(progress);
   };
 
-  const onChangeVideoProgress = () => {
-    const progress = player.current.currentTime / player.current.duration / 100;
+  const onChangeVideoProgress = (e) => {
+    const updatedProgressWidth = e.target.value;
+    const updatedProgress = (updatedProgressWidth / 100) * player.current.duration;
+    const updatedProgressText = getTimeAsMinSec(updatedProgress);
 
-    return progress;
+    player.current.currentTime = updatedProgress;
+    setProgressBarWidth(updatedProgressWidth);
+    setVideoProgressText(updatedProgressText);
+  };
+
+  const onSkipByDuration = (skipBy) => {
+    const updatedCurrentTime = player.current.currentTime + skipBy;
+    const progressWidth = (updatedCurrentTime / player.current.duration) * 100;
+    const progressText = getTimeAsMinSec(updatedCurrentTime);
+
+    if (updatedCurrentTime >= player.current.duration) {
+      player.current.currentTime = player.current.duration;
+      setIsPlaying(false);
+      setProgressBarWidth(100);
+      setVideoProgressText(videoDurationText);
+      return;
+    }
+
+    if (updatedCurrentTime <= 0) {
+      player.current.currentTime = 0;
+      setProgressBarWidth(0);
+      setVideoProgressText('00:00');
+      return;
+    }
+
+    player.current.currentTime = updatedCurrentTime;
+    setProgressBarWidth(progressWidth);
+    setVideoProgressText(progressText);
   };
 
   return (
@@ -73,7 +109,7 @@ function VideoPlayer() {
         preload="true"
         onClick={onTogglePlay}
         onLoadedMetadata={onLoadVideo}
-        onTimeUpdate={onUpdateProgress}>
+        onTimeUpdate={onUpdateProgressBar}>
         <source
           src="https://tra-ww000-cp.akamaized.net/HGO-TW-001-A1881/videos/misc/mp4/trailer/HGO-TW-001-A1881-Z05-406p-800k.mp4"
           type="video/mp4"
@@ -86,25 +122,27 @@ function VideoPlayer() {
           {isPlaying ? <IoPauseOutline /> : <IoPlayOutline />}
         </ControlBtn>
 
-        <ControlBtn type="button" controlType="backwards">
+        <ControlBtn type="button" onClick={() => onSkipByDuration(-10)}>
           <BsArrowCounterclockwise />
         </ControlBtn>
 
-        <ControlBtn type="button" controlType="forwards">
+        <ControlBtn type="button" onClick={() => onSkipByDuration(10)}>
           <BsArrowClockwise />
         </ControlBtn>
 
         <ProgressBarCont>
-          <ProgressBarText>{videoProgress}</ProgressBarText>
+          <ProgressBarText>{videoProgressText}</ProgressBarText>
           <ProgressBarInput
             type="range"
             min="0"
             max="100"
             step="1"
             value={progressBarWidth}
+            progressBarWidth={`${progressBarWidth}%`}
+            bufferBarWidth={`${bufferBarWidth}%`}
             onChange={onChangeVideoProgress}
           />
-          <ProgressBarText>{videoDuration}</ProgressBarText>
+          <ProgressBarText>{videoDurationText}</ProgressBarText>
         </ProgressBarCont>
 
         <ControlBtn type="button" onClick={onToggleFullScreen}>
--- src/components/VideoPlayer/index.styled.js
+++ src/components/VideoPlayer/index.styled.js
@@ -52,8 +52,121 @@ export const ProgressBarText = styled.span`
   color: #fff;
   font-size: 12px;
   padding: 0 8px;
+  letter-spacing: 1px;
 `;
 
 export const ProgressBarInput = styled.input`
   flex: 1;
+
+  &[type='range'] {
+    -webkit-appearance: none;
+    width: 100%;
+    background: transparent;
+    position: relative;
+
+    &:focus {
+      outline: transparent;
+    }
+
+    // track
+    &::-webkit-slider-runnable-track {
+      background-color: #6e6e6e;
+      border-radius: 20px;
+      height: 8px;
+    }
+
+    &::-moz-range-track {
+      background-color: #6e6e6e;
+      border-radius: 20px;
+      height: 8px;
+    }
+
+    &::-ms-track {
+      background-color: #6e6e6e;
+      border-radius: 20px;
+      height: 8px;
+    }
+
+    // thumb
+    &::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      height: 16px;
+      width: 16px;
+      border-radius: 50%;
+      background: #fff;
+      margin: -4px 0;
+      z-index: 3;
+      position: relative;
+      cursor: pointer;
+    }
+
+    &::-moz-range-thumb {
+      height: 16px;
+      width: 16px;
+      border-radius: 50%;
+      background: #fff;
+      margin: -4px 0;
+      z-index: 3;
+      position: relative;
+      cursor: pointer;
+    }
+
+    &::-ms-thumb {
+      height: 18px;
+      width: 18px;
+      border-radius: 50%;
+      background: #fff;
+      margin: -4px 0;
+      z-index: 3;
+      position: relative;
+      cursor: pointer;
+    }
+
+    // progress
+    &::before {
+      content: '';
+      width: ${({ progressBarWidth }) => progressBarWidth};
+      height: 8px;
+      background-color: #f26f21;
+      border-top-left-radius: 20px;
+      border-bottom-left-radius: 20px;
+      position: absolute;
+      top: 0;
+      z-index: 2;
+    }
+
+    &::-moz-range-progress {
+      width: ${({ progressBarWidth }) => progressBarWidth};
+      height: 8px;
+      background-color: #f26f21;
+      border-top-left-radius: 20px;
+      border-bottom-left-radius: 20px;
+      position: absolute;
+      top: 0;
+      z-index: 2;
+    }
+
+    &::-ms-fill-lower {
+      width: ${({ progressBarWidth }) => progressBarWidth};
+      height: 8px;
+      background-color: #f26f21;
+      border-top-left-radius: 20px;
+      border-bottom-left-radius: 20px;
+      position: absolute;
+      top: 0;
+      z-index: 2;
+    }
+
+    // buffer
+    &::after {
+      content: '';
+      width: ${({ bufferBarWidth }) => bufferBarWidth};
+      height: 8px;
+      background-color: #dcdcdc;
+      border-radius: 20px;
+      position: absolute;
+      top: 0;
+      z-index: 1;
+    }
+  }
 `;
