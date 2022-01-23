a8eafb842d9c6287609c6d1ea8cddb50a4602a71
houy062924 <47458867+houy062924@users.noreply.github.com>
2022-01-23 23:25:41 +0800

    Update: add hint to PowerBtn on first load

--- src/App.js
+++ src/App.js
@@ -1,67 +1,12 @@
-import { useState } from 'react';
 import TvScreen from './components/TvScreen';
-import VideoPlayer from './components/VideoPlayer/index';
-import VideoPlayerSettings from './components/VideoPlayerSettings/index';
-import { defaultProps } from './props';
 import { GlobalStyle } from './Global.styled';
 import { AppCont } from './App.styled';
 
 function App() {
-  const [settings, setSettings] = useState(defaultProps);
-
-  const onChangeSettings = (event) => {
-    const { name, value, checked, type } = event.target;
-
-    setSettings((prevSettings) => ({
-      ...prevSettings,
-      [name]: type !== 'checkbox' ? value : checked,
-    }));
-  };
-
-  const onResetSettings = () => {
-    setSettings(defaultProps);
-  };
-
-  const onPlay = () => {
-    console.log('Video has started playing!');
-  };
-
-  const onPause = () => {
-    console.log('Video has been paused!');
-  };
-
-  const onEnd = () => {
-    console.log('Video has ended!');
-  };
-
-  const onSeek = (seekedTime) => {
-    console.log(`Video progress has been changed to ${seekedTime}!`);
-  };
-
-  const onSkipped = (skipBy) => {
-    console.log(`Video has been skipped ${skipBy} seconds!`);
-  };
-
   return (
     <AppCont>
       <GlobalStyle />
-
       <TvScreen />
-      {/* <VideoPlayer
-        {...settings}
-        playerContClassName="player-cont"
-        onPlayCallback={onPlay}
-        onPauseCallback={onPause}
-        onEndCallback={onEnd}
-        onSeekCallback={onSeek}
-        onSkippedCallback={onSkipped}
-      />
-
-      <VideoPlayerSettings
-        {...settings}
-        onChangeSettings={onChangeSettings}
-        onResetSettings={onResetSettings}
-      /> */}
     </AppCont>
   );
 }
